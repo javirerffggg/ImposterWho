06211fe2f737a48e299cfa9425c67b56314683ac
javirerffggg <160790690+javirerffggg@users.noreply.github.com>
2025-08-26 18:39:24 +0000

    Create words.js

---
+++ words.js
@@ -0,0 +1,52 @@
+const WORD_CATEGORIES = {
+    "Animales de Granja": {
+        "easy": ["Vaca", "Cerdo", "Gallina", "Oveja", "Caballo"],
+        "medium": ["Ganso", "Pavo", "Cabra", "Burro", "Conejo"],
+        "hard": ["Codorniz", "Alpaca", "Faisán", "Avestruz", "Llama"]
+    },
+    "Frutas": {
+        "easy": ["Manzana", "Plátano", "Naranja", "Fresa", "Uva"],
+        "medium": ["Mango", "Piña", "Kiwi", "Cereza", "Pera"],
+        "hard": ["Lichi", "Maracuyá", "Carambola", "Pitahaya", "Rambután"]
+    },
+    "Instrumentos Musicales": {
+        "easy": ["Guitarra", "Piano", "Batería", "Violín", "Flauta"],
+        "medium": ["Saxofón", "Trompeta", "Arpa", "Clarinete", "Bajo"],
+        "hard": ["Oboe", "Fagot", "Theremín", "Balalaica", "Djembe"]
+    },
+    "Deportes": {
+        "easy": ["Fútbol", "Baloncesto", "Tenis", "Béisbol", "Natación"],
+        "medium": ["Voleibol", "Rugby", "Boxeo", "Golf", "Ciclismo"],
+        "hard": ["Esgrima", "Bádminton", "Curling", "Críquet", "Polo acuático"]
+    },
+    "Partes del Cuerpo": {
+        "easy": ["Mano", "Pie", "Ojo", "Nariz", "Boca"],
+        "medium": ["Codo", "Rodilla", "Hombro", "Pulmón", "Corazón"],
+        "hard": ["Páncreas", "Timo", "Hipófisis", "Yunque", "Tráquea"]
+    },
+    "Profesiones": {
+        "easy": ["Médico", "Profesor", "Policía", "Bombero", "Cocinero"],
+        "medium": ["Arquitecto", "Abogado", "Ingeniero", "Periodista", "Enfermero"],
+        "hard": ["Actuario", "Geólogo", "Entomólogo", "Sommelier", "Topógrafo"]
+    },
+    "Países": {
+        "easy": ["España", "México", "Francia", "Italia", "China"],
+        "medium": ["Argentina", "Japón", "Canadá", "Brasil", "Alemania"],
+        "hard": ["Liechtenstein", "Kiribati", "Bután", "Esuatini", "Yibuti"]
+    },
+    "Películas Famosas": {
+        "easy": ["Titanic", "Star Wars", "El Rey León", "Harry Potter", "Jurassic Park"],
+        "medium": ["Pulp Fiction", "Forrest Gump", "El Padrino", "Matrix", "Avatar"],
+        "hard": ["Ciudadano Kane", "2001: Odisea del Espacio", "Casablanca", "El Séptimo Sello", "Rashomon"]
+    },
+    "Marcas de Coches": {
+        "easy": ["Ford", "Toyota", "Volkswagen", "Honda", "BMW"],
+        "medium": ["Mercedes-Benz", "Audi", "Ferrari", "Porsche", "Tesla"],
+        "hard": ["Koenigsegg", "Pagani", "Bugatti", "Maserati", "Spyker"]
+    },
+    "Superhéroes": {
+        "easy": ["Superman", "Batman", "Spiderman", "Iron Man", "Capitán América"],
+        "medium": ["Thor", "Hulk", "Wonder Woman", "Flash", "Aquaman"],
+        "hard": ["Doctor Strange", "Ant-Man", "Black Panther", "Martian Manhunter", "Shazam"]
+    }
+};
